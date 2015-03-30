fb5ccfd962b8ce9e906bdf8daf0289fe0c5bf857
Lucas de Castro Oliveira <lcastro.oliveira@acm.org>
2015-03-30 16:03:08 -0300

    SECURITY MODULE 4 READY
    Added username and password onto the dabase. Configured on the .xml
    file. Also there is a script file on resources that was run in order to
    create the authentication tables.

---
+++ src/main/resources/Create Users Fitness tracker.sql
@@ -0,0 +1,19 @@
+create table users(username varchar (50) not null primary key,
+	password varchar (50) not null,
+    enabled boolean null);
+    
+create table authorities (
+	username varchar(50) not null,
+    authority varchar (50) not null,
+    constraint fk_authorities_users
+    foreign key(username) references users(username));
+    create unique index ix_auth_username on authorities
+    (username,authority);
+    
+insert into users(username,password, enabled) values ("lucas","secret",true);
+
+select * from users;
+
+insert into authorities (username,authority) values ("lucas", "ROLE_USER");
+
+select * from authorities;
